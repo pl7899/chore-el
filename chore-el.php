431754aafbb6700c94325fe03a124b157dbbfdcd
pl7899 <pl7899@ymail.com>
2025-01-30 12:46:41 -0500

    fix for php file path

--- chore-el.php
+++ chore-el.php
@@ -56,7 +56,7 @@
 	</div>
 
 	<div class="form-popup" id="myForm">
-		<form action="/add_chore.php" class="form-container">
+		<form action="chore-el/add_chore.php" class="form-container">
 			<h1>Chore Name</h1>
 
 			<label for="name"><b>name</b></label>
