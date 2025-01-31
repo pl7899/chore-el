0314580f885a132248df84abb58f496801e46a74
pl7899 <pl7899@ymail.com>
2025-01-30 21:18:42 -0500

    move funxtion call into scrript

--- add_chore.php
+++ add_chore.php
@@ -1,9 +1,10 @@
 
-<script src="chore-el_shim.js"></script>
+<script src="chore-el_shim.js">
+	createNewChore($choreName, $choreFreq);
+</script>
 <?php
 
 // should be able to take action for commiting to the DB then use the below line to jump back to the main page
-createNewChore($choreName, $choreFreq);
 
 header("Location: https://www.northridge-studios.com/chore-el/chore-el.php", true, 301);  
 exit();  
