3f52b13fb851d02c90672df2c7fb661b5a63019d
pl7899 <pl7899@ymail.com>
2025-01-31 13:36:01 -0500

    last try on this

--- add_chore.php
+++ add_chore.php
@@ -1,3 +1,11 @@
+<?php
+
+// should be able to take action for commiting to the DB then use the below line to jump back to the main page
+createNewChore($_POST['choreName'], $_POST['choreFreq']);
+
+header("Location: https://www.northridge-studios.com/chore-el/chore-el.php", true, 301);  
+exit();  
+?>
 
 <script>
 
@@ -11,11 +19,3 @@ function createNewChore(choreName, choreFreq) {
 }
 
 </script>
-<?php
-
-// should be able to take action for commiting to the DB then use the below line to jump back to the main page
-createNewChore($_POST['choreName'], $_POST['choreFreq']);
-
-header("Location: https://www.northridge-studios.com/chore-el/chore-el.php", true, 301);  
-exit();  
-?>
